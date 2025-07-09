b7e18e82227f0aa6ab3b68755f925a8e466c90be
kehinde olorunda <kehindeolorunda1000@gmail.com>
2025-07-09 11:31:47 +0100

    UPDATE CHANGES

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 // welcome message component
 
@@ -31,6 +32,13 @@ function App() {
       <MainContent />
       <Footer />
       </>
+       <>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+    </>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

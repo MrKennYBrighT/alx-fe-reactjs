bf8680f20293568a51eee46716e9f343ed6080dd
kehinde olorunda <kehindeolorunda1000@gmail.com>
2025-07-14 11:55:40 +0100

    update changes

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@
    return (
      <div style={{border: '1px solid gray', padding: '10px', margin: '10px'}}>
        <h2 style={{color: 'blue'}}>{props.name}</h2>
-       <p style={{color: 'red'}}>Age: {props.age}</p>
+       <p>Age: <span style={{fontWeight: 'bold', color: 'red'}}></span>{props.age}</p>
        <p style={{color: 'yellow'}}>Bio: {props.bio}</p>
      </div>
    );

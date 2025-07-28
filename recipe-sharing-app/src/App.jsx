52d55ff1c072478af9cf92a6413e4613ca740f43
kehinde olorunda <kehindeolorunda1000@gmail.com>
2025-07-28 14:04:09 +0100

    FINAL UPDATE FOR TASK 1

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetail from './components/RecipeDetails'; // ✅ corrected filename
 
 const App = () => {
   return (
-    <div style={{ padding: '1rem' }}>
-      <h2>🍽️ Recipe Sharing App</h2>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipes/:recipeId" element={<RecipeDetail />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div style={{ padding: '1rem' }}>
+        <h2>🍽️ Recipe Sharing App</h2>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipes/:recipeId" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 

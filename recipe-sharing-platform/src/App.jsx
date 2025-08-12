dd4dde066137419945fe55974d19ee4f4156ae69
kehinde olorunda <kehindeolorunda1000@gmail.com>
2025-08-12 11:26:48 +0100

    update

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './components/HomePage';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +28,9 @@ function App() {
         <h1 class="text-red-500 font-extrabold italic text-4xl">Tailwind is working!</h1>
 
       </div>
+          <div className="min-h-screen bg-gray-100">
+      <HomePage />
+    </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>

9149abfcf36eccc6b224768690d6cca39f1c5226
kehinde olorunda <kehindeolorunda1000@gmail.com>
2025-08-19 13:54:44 +0100

    REACT TO-DO LIST

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import TodoList from '../components/TodoList';
+
+test('renders initial todos', () => {
+  render(<TodoList />);
+  expect(screen.getByText('Learn React')).toBeInTheDocument();
+  expect(screen.getByText('Write Tests')).toBeInTheDocument();
+});
+
+test('adds a new todo', () => {
+  render(<TodoList />);
+  const input = screen.getByPlaceholderText('Add new todo');
+  const button = screen.getByText('Add');
+
+  fireEvent.change(input, { target: { value: 'New Task' } });
+  fireEvent.click(button);
+
+  expect(screen.getByText('New Task')).toBeInTheDocument();
+});
+
+test('toggles todo completion', () => {
+  render(<TodoList />);
+  const todoItem = screen.getByText('Learn React');
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle('text-decoration: line-through');
+});
+
+test('deletes a todo', () => {
+  render(<TodoList />);
+  const deleteButton = screen.getAllByText('Delete')[0];
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText('Learn React')).not.toBeInTheDocument();
+});
